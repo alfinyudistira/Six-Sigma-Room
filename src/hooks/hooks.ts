465eeed1f4895434416386c714aa0bca42a756e0
ALFIN MAULANA YUDISTIRA <alvinyoalvin44@gmail.com>
2026-04-05 10:36:30 +0700

    Delete src/hooks/hooks.ts

--- src/hooks/hooks.ts
+++
@@ -1,8 +0,0 @@
-// src/hooks/hooks.ts
-// Convenience barrel — pages can import from here instead of individual files
-export { useURLState } from './useURLState'
-export { useDebounce, useDebouncedCallback } from './useDebounce'
-export { useModulePersist } from './useModulePersist'
-export { useHaptic } from './useHaptic'
-export { useCurrency } from './useCurrency'
-export { useKeyboardNav } from './useKeyboardNav'
